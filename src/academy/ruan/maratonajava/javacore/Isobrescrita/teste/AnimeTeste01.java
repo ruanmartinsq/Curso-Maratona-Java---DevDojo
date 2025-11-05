805b44620afc3f6e6c4480f0e29c8bb32bb544a5
ruanmartinsq <ruanlana34@gmail.com>
2025-11-05 10:25:06 -0300

    Sobrescrita do metodo string

---
+++ src/academy/ruan/maratonajava/javacore/Isobrescrita/teste/AnimeTeste01.java
@@ -0,0 +1,10 @@
+package academy.ruan.maratonajava.javacore.Isobrescrita.teste;
+
+import academy.ruan.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTeste01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("One punch man");
+        System.out.println(anime);
+    }
+}
